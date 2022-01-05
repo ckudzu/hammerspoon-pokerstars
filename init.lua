f010759e4353ab04cae085b70bc8b803523b2d21
kudzu <ckudzu@gmail.com>
2022-01-06 02:58:40 +0900

    add window switcher with page up/down

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
+logger = hs.logger.new("init", 5)
+
 SCROLL_DELTA = 1
 
 --size 950 * 659
@@ -26,8 +28,8 @@ RAISE_TOP_PCT = 0.81
 types = hs.eventtap.event.types
 
 function click(pos)
-    local msg = "clicking:" .. pos.x .. " " .. pos.y
-    hs.notify.new({title="Hammerspoon", informativeText=msg}):send()
+--    local msg = "clicking:" .. pos.x .. " " .. pos.y
+--    hs.notify.new({title="Hammerspoon", informativeText=msg}):send()
     hs.eventtap.event.newMouseEvent(types.mouseMoved, pos):post()
     hs.timer.usleep(10)
     hs.eventtap.event.newMouseEvent(types.leftMouseDown, pos):post()
@@ -49,6 +51,25 @@ end
 function clickRelativeWindowPos(xpct, ypct)
     click(getRelativePosInWindow(xpct, ypct))
 end
+
+hs.hotkey.bind({"cmd"}, "7", function()
+    local window = hs.window.focusedWindow()
+    local windowPos = window:topLeft();
+    local windowSize = window:size()
+    local screen = hs.screen.mainScreen()
+    local snap = screen:snapshot(hs.geometry.rect(windowPos.x, windowPos.y, windowSize.w, windowSize.h))
+    local url = snap:encodeAsURLString()
+    hs.notify.new({title="Hammerspoon", informativeText=url}):send()
+    logger:d(url)
+    hs.pasteboard.setContents(url)
+    local webview = hs.webview.new(hs.geometry.rect(100, 100, 400, 400 * windowSize.h / windowSize.w))
+    webview:url(url)
+    webview:windowStyle("titled")
+    webview:windowStyle("resizable")
+    webview:windowStyle("utility")
+    webview:windowStyle("HUD")
+    webview:show()
+end)
 
 psKeys = {
   hs.hotkey.bind({"cmd"}, ",", function()
@@ -77,7 +98,24 @@ psKeys = {
     clickRelativeWindowPos(0.5, 0.5)
     hs.eventtap.scrollWheel({0, SCROLL_DELTA}, {});
   end),
+  hs.hotkey.bind({}, "pageup", function()
+    focusPsWindow(1)
+  end),
+  hs.hotkey.bind({}, "pagedown", function()
+    focusPsWindow(-1)
+  end),
 }
+
+function focusPsWindow(offset)
+  pswindows = hs.window.filter.new{"PokerStars"}:getWindows(hs.window.filter.sortByCreated)
+  for index = 1, #pswindows do
+    if hs.window.focusedWindow() == pswindows[index] then
+      focusIndex = math.fmod(index -1 + offset + #pswindows, #pswindows) + 1
+      pswindows[focusIndex]:focus()
+      return
+    end
+  end
+end
 
 function setEnabled(enabled)
   --hs.notify.new({title=appName, informativeText="Change state"}):send()
@@ -102,4 +140,26 @@ watcher = hs.application.watcher.new(function(appName, eventType, app)
     end
   end
 end)
+
+local clicker_pos
+clicker = hs.timer.new(1, function()
+  logger:d("timer")
+  for i = 1, 400 do
+    hs.eventtap.event.newMouseEvent(types.leftMouseDown, clicker_pos):post()
+    hs.eventtap.event.newMouseEvent(types.leftMouseUp, clicker_pos):post()
+  end    
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "a", function()
+  if clicker:running() then
+    logger:d("stop")
+    clicker:stop()
+  else
+    clicker_pos = hs.mouse.getAbsolutePosition()
+    logger:d("start")
+    clicker:start()
+    clicker:fire()
+  end
+end)
+
 watcher:start()
